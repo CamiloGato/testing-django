3d4f290cae665572c4716ec49e1ed0be05a27635
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-02-21 13:53:42 -0500

    Update Unit Test
    Starting Unit Tests

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -2,15 +2,20 @@ from django.test import TestCase
 
 from selenium import webdriver
 
-class FunctionalTestCase(TestCase):
+# class FunctionalTestCase(TestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Chrome()
+#     def setUp(self):
+#         self.browser = webdriver.Chrome()
 
-    def test_there_is_homepage(self):
-        self.browser.get('http://localhost:8000')
-        self.assertIn('install', self.browser.page_source)
+#     def test_there_is_homepage(self):
+#         self.browser.get('http://localhost:8000')
+#         self.assertIn('install', self.browser.page_source)
 
-    def tearDown(self):
-        self.browser.quit()
+#     def tearDown(self):
+#         self.browser.quit()
     
+class UnitTestCase(TestCase):
+    
+    def test_home_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'hashing/home.html')
