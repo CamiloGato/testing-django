e83cca3f8c0bf88371b2d49ed10d82c9b729ddbb
Camilo Andrés Rojas Hernández <93359519+Gaussito@users.noreply.github.com>
2022-02-21 15:19:35 -0500

    Unit Test
    Aditional unit tests

--- hashthat/hashing/tests.py
+++ hashthat/hashing/tests.py
@@ -1,19 +1,30 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+
 from selenium import webdriver
+import hashlib
 
 from .forms import HashForm
+from .models import Hash
 
-# class FunctionalTestCase(TestCase):
+class FunctionalTestCase(TestCase):
 
-#     def setUp(self):
-#         self.browser = webdriver.Chrome()
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-#     def test_there_is_homepage(self):
-#         self.browser.get('http://localhost:8000')
-#         self.assertIn('install', self.browser.page_source)
+    def test_there_is_homepage(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Enter hash Here: ', self.browser.page_source)
 
-#     def tearDown(self):
-#         self.browser.quit()
+    def test_hash_of_hello(self):
+        self.browser.get('http://localhost:8000')
+        text = self.browser.find_element_by_id('id_text')
+        text.send_keys('hello')
+        self.browser.find_element_by_name('submit').click()
+        self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
+
+    def tearDown(self):
+        self.browser.quit()
     
 class UnitTestCase(TestCase):
     
@@ -24,3 +35,31 @@ class UnitTestCase(TestCase):
     def test_hash_form(self):
         form = HashForm(data={'text':'hello'})
         self.assertTrue(form.is_valid())
+
+    def test_hash_func_works(self):
+        text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+        self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824', text_hash)
+
+    def saveHash(self):
+        hash = Hash()
+        hash.text = 'hello'
+        hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+        hash.save()
+        return hash
+
+    def test_hash_object(self):
+        hash = self.saveHash()
+        pulled_hash = Hash.objects.get(hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertEqual(hash.text, pulled_hash.text)
+
+    def test_viewing_hash(self):
+        hash = self.saveHash()
+        response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+        self.assertContains(response, 'hello')
+
+    def test_bad_data(self):
+        def badHash():
+            hash = Hash()
+            hash.hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824aaaaaaaa'
+            hash.full_clean()
+        self.assertRaises(ValidationError, badHash)
--- hashthat/hashing/views.py
+++ hashthat/hashing/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import HashForm
+from .models import Hash
+
+import hashlib
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'hashing/home.html')
+
+    if request.method == "POST":
+        filled_form = HashForm(request.POST)
+        if filled_form.is_valid():
+            text = filled_form.cleaned_data['text']
+            text_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
+            try:
+                Hash.objects.get(hash=text_hash)
+            except Hash.DoesNotExist:
+                hash = Hash()
+                hash.text = text
+                hash.hash = text_hash
+                hash.save()
+            
+            return redirect('hash', hash=text_hash)
+
+    form = HashForm()
+    context = {'form':form}
+    return render(request, 'hashing/home.html', context)
+
+def hash(request, hash):
+    hash = Hash.objects.get(hash=hash)
+    context = {'hash':hash}
+    return render(request, 'hashing/hash.html', context)
